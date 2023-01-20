2773d7749c7be094afeaa52fa76ec8c90b1e8db2
Kerriea-star <ojared511@gmail.com>
2023-01-20 14:29:45 +0300

    fixes

--- training.py
+++ training.py
@@ -2,18 +2,14 @@ import random
 import numpy as np
 import json
 import pickle
-
 import nltk
-# nltk.download('all')
-from nltk.stem import WordNetLemmatizer
-
 import tensorflow
-
+from nltk.stem import WordNetLemmatizer
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open("intents.json").read())
 
@@ -25,10 +21,11 @@ ignore_letters = ["?","!", ".", ","]
 for intent in intents["intents"]:
     for pattern in intent["patterns"]:
         word_list = nltk.word_tokenize("pattern")
-        words.append(word_list)
+        words.extend(word_list)
         documents.append((word_list, intent["tag"]))
-
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+print(words)
