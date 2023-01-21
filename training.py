5ad0e338f65ebbfbb7c7c370950ef557c5b60034
Kerriea-star <ojared511@gmail.com>
2023-01-21 14:54:33 +0300

    fixes

--- training.py
+++ training.py
@@ -82,6 +82,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 #fitting and saving the model
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save("chatbot_model.model")
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbotmodel.h5', hist)
 print("model created")
